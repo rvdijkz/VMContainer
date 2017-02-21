c06077733808ce3738b2e0e6950fd04a27b4a11d
Gebruiker@PC-Zolder <rvdijkz@gmail.com>
2017-02-21 20:42:36 +0100

    add git add .

--- src/main/java/rvdijkz/nl/Main.java
+++ src/main/java/rvdijkz/nl/Main.java
@@ -2,7 +2,7 @@ package rvdijkz.nl;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello again.... bla");
+        System.out.println("Hello again.... bla!!");
     }
 
 }
